f1b7225b05be66c450122dfd9cb7a531ef181f27
Aditya Tomar <adityatomar80050@gmail.com>
2025-02-05 11:58:41 +0530

    Commit

---
+++ bitwise_operator.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+    int a=1 , b=0;
+
+    cout << (a & b) << endl;
+
+    cout << (a | b) << endl;
+
+    cout << (1 ^ 0) << endl;
+
+    cout << (1 ^ 1) << endl;
+
+    cout << (0 ^ 0) << endl;
+
+    cout << (10 << 2) << endl;
+
+    cout << (10 >> 2) << endl;
+
+    return 0;
+}
---
+++ data_tyes_size.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+    cout << sizeof(long long int) << "\n";
+
+    return 0;
+}
